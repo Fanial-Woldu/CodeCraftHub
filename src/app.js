85082c98ac94bce73a7ab6111b8d6580c27f0ccc
Fanial-Woldu <Fanial.woldu@mnsu.edu>
2025-10-28 21:17:24 -0500

    Adding temporary changes to Github

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 // src/app.js
 require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/db');
 
+const connectDB = require('./config/db');
+const initServer = require('./config/server');
 const userRoutes = require('./routes/userRoutes');
 const errorHandler = require('./utils/errorHandler');
-const initServer = require('./config/server');
-const app = require('./config/server');
-const PORT = process.env.PORT || 5000;
+
+const app = initServer();
+console.log('Type of app:', typeof app);
 connectDB();
+
 app.use('/api/users', userRoutes);
 app.use(errorHandler);
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
